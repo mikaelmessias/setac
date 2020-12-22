44e6f56b81fb09e72229cad55aec43d2a8fcde5b
Mikael Messias <mikaelmessias@outlook.com>
2020-12-22 13:29:14 -0300

    Adiciona correções
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- js/scripts-setac.js
+++ js/scripts-setac.js
@@ -5,15 +5,15 @@ window.addEventListener('scroll', function resizeHeaderOnScroll() {
     navbar = document.getElementById('nav-fixa');
     logo = document.getElementById("logo-setac");
 
-    if(navbar){
-        if(tamanhoTela >= 768){
-            if (distanceY > shrinkOn){
+    if (navbar) {
+        if (tamanhoTela >= 768) {
+            if (distanceY > shrinkOn) {
                 navbar.style.padding = "0";
-                
+
                 logo.style.color = "rgba(0,132,255,1)";
-            }else{
-                navbar.style.padding = "20px 0";
-                
+            } else {
+                navbar.style.padding = "0";
+
                 logo.style.color = "rgba(0,132,255,1)";
             }
         }
@@ -21,44 +21,44 @@ window.addEventListener('scroll', function resizeHeaderOnScroll() {
 });
 
 function contagemRegressiva() {
-    var YY = 2019;
+    var YY = 2020;
     var MM = 10;
     var DD = 21;
     var HH = 08;
     var MI = 00;
-    var SS = 00; 
+    var SS = 00;
 
-    var hoje = new Date();  
-    var futuro = new Date(YY,MM-1,DD,HH,MI,SS); 
+    var hoje = new Date();
+    var futuro = new Date(YY, MM - 1, DD, HH, MI, SS);
 
-    var ss = parseInt((futuro - hoje) / 1000);  
-    var mm = parseInt(ss / 60);  
-    var hh = parseInt(mm / 60);  
-    var dd = parseInt(hh / 24);   
-    ss = ss - (mm * 60);  
-    mm = mm - (hh * 60);  
-    hh = hh - (dd * 24);   
+    var ss = parseInt((futuro - hoje) / 1000);
+    var mm = parseInt(ss / 60);
+    var hh = parseInt(mm / 60);
+    var dd = parseInt(hh / 24);
+    ss = ss - (mm * 60);
+    mm = mm - (hh * 60);
+    hh = hh - (dd * 24);
 
-    if(dd < 0) {
-	dd = 0;
+    if (dd < 0) {
+        dd = 0;
     }
 
-    if(hh < 0) {
-	hh = 0;
+    if (hh < 0) {
+        hh = 0;
     }
 
-    if(mm < 0) {
-	mm = 0;
+    if (mm < 0) {
+        mm = 0;
     }
 
-    if(ss < 0) {
-	ss = 0;
+    if (ss < 0) {
+        ss = 0;
     }
-    
+
     document.getElementById('cont-dias').innerHTML = dd;
-    document.getElementById('cont-horas').innerHTML = hh;  
-    document.getElementById('cont-minutos').innerHTML = mm;  
-    document.getElementById('cont-segundos').innerHTML = ss;  
+    document.getElementById('cont-horas').innerHTML = hh;
+    document.getElementById('cont-minutos').innerHTML = mm;
+    document.getElementById('cont-segundos').innerHTML = ss;
 
-	setTimeout(contagemRegressiva,1000);
+    setTimeout(contagemRegressiva, 1000);
 }
